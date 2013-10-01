cb93274b5beb484ae6e07e3efc60651572d816d6
Moshe Bildner <moshe.bildner@gmail.com>
2013-10-01 12:23:32 -0400

    added a function to get the intersection of multiple arrays. This is an intermediate step to reading records from the database:
    1) query database with an object
    2) check to see whether each key in the object is indexed
    3) find every uid that matches any index
    4) find the intersection - ie find every uid that matches EVERY index
    5) retrieve records from the datastore that match those uids

---
+++ jsworkshop.js
@@ -0,0 +1,11 @@
+var arrayFilter(arrayList){
+	// find and return any item in each of the arrays passed in initially
+
+	var hits = [];
+
+	for (i in arrayList){
+		for (j in i){
+			alert(j);
+		}
+	}
+}
