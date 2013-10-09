9a73efaf608c91b4475b6d51a4d37d6b51b041fd
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-10-09 16:55:04 -0400

    Can just directly say file in current folder

--- testing.js
+++ testing.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 var LocalStorage = require('node-localstorage').LocalStorage;
-var jscrud = require(path.join(path.dirname(module.filename), 'jscrud.js'));
+var jscrud = require('./jscrud.js');
 
 var localStorage = new LocalStorage('./scratch');
 var db = new jscrud.Jscrud(localStorage);
